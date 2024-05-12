947622ca85ea75afdf6e4763f772ade5ff6807e0
jehob <jehob@web.de>
2024-05-12 21:52:10 +0200

    added tmux config with some plugins

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -81,4 +81,16 @@
     };
   };
 
+
+  programs.tmux = {
+    enable = true;
+    prefix = "C-a";
+    historyLimit = 50000;
+    escapeTime = 20;
+    plugins = with pkgs;
+      [
+        tmuxPlugins.sensible
+        tmuxPlugins.resurrect
+      ];
+  };
 }
