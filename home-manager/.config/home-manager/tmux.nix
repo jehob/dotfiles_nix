ebca02ab82280a1d9a4031537f9a4c1de546b32f
jehob <jehob@web.de>
2024-05-18 18:24:45 +0200

    added dracula theme to tmux

--- home-manager/.config/home-manager/tmux.nix
+++ home-manager/.config/home-manager/tmux.nix
@@ -6,11 +6,24 @@
     prefix = "C-a";
     historyLimit = 50000;
     escapeTime = 20;
-    plugins = with pkgs;
+    keyMode = "vi";
+
+    plugins = with pkgs.tmuxPlugins;
       [
-        tmuxPlugins.sensible
-        tmuxPlugins.resurrect
-	tmuxPlugins.nord
+        sensible
+        resurrect
+        yank
+        {
+          plugin = dracula;
+          extraConfig = ''
+            set -g @dracula-plugins "ssh-session"
+            set -g @dracula-refresh-rate 10
+            set -g @dracula-show-left-icon session
+          '';
+	}
       ];
+    extraConfig = ''
+      set -g mouse on
+    '';
   };
 }
